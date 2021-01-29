573169f3c2fa72697df76029ec80e33f2bd92288
Jerry <731238433@qq.com>
2021-01-29 23:40:30 +0800

    '完成私钥删除'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,4 @@
 // index.js
-// 获取应用实例
-// const app = getApp()
 const util = require('../../utils/util.js')
 let time = null
 Page({
@@ -8,7 +6,11 @@ Page({
     // 存储私钥列表
     codeList: [],
     // 存储倒计时
-    time: 30 - parseInt(Date.now() % 30000 / 1000)
+    time: 30 - parseInt(Date.now() % 30000 / 1000),
+    // 保存编辑序列
+    editIndex: 0,
+    // 删除model控制变量
+    showDelAccountModel: false
   },
   onLoad() {
     wx.showLoading({
@@ -57,7 +59,6 @@ Page({
       success (res) {
         const query = util.getQueryObjectByUrl(res.result)
         let codeList = that.data.codeList
-
         // 判断当前私钥是否存在
         if (util.arrayIsExistKeyValue(codeList, 'secret', query.secret)) {
           wx.showToast({
@@ -67,16 +68,57 @@ Page({
           })
           return
         }
-
+        // 添加并保存本地
         codeList.push(query)
         that.setData({
           codeList: codeList
         })
         that.setStorage()
+        // 重置定时器
         that.startTime()
       }
     })
   },
+  // 长按编辑
+  handleLongPress (e) {
+    const index = e.currentTarget.dataset.value
+    this.setData({
+      editIndex: index
+    })
+    const that = this
+    wx.showActionSheet({
+      itemList: ['编辑', '删除'],
+      success (res) {
+        if (res.tapIndex === 0) {
+          // 编辑
+        }
+        if (res.tapIndex === 1) {
+          // 删除
+          that.setData({
+            showDelAccountModel: true
+          })
+        }
+      }
+    })
+  },
+  // 删除秘钥
+  delAccount() {
+    let codeList = this.data.codeList
+    codeList.splice(this.data.editIndex, 1)
+    this.setData({
+      codeList: codeList
+    })
+    this.setStorage()
+    this.startTime()
+    this.clearDelAccountModel()
+  },
+  // 隐藏model
+  clearDelAccountModel() {
+    this.setData({
+      showDelAccountModel: false
+    })
+  },
+  // 页面卸载销毁定时器
   onUnLoad() {
     time = null
   }
